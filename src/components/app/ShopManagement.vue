09f3be10d4ab48c387bc9af127ae42bba925f266
Muhammad Waqas <waqas4400@gmail.com>
2020-08-28 15:09:22 +0500

    made new vue component shopmanagement (working)

---
+++ src/components/app/ShopManagement.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row p-1">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 mb-2 mt-2">
+            <button class="btn btn-success" v-on:Click="this.getShops">
+                Refresh
+            </button>
+            <button class="btn btn-success" v-on:Click="this.getConsole">
+                Console
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+import ShopService from '../../db/ShopService'
+export default {
+    data () {
+        return {
+            rowData: [],
+            shopService: new ShopService()
+        }
+    },
+    methods: {
+        getShops () {
+            this.shopService.getShops().then(successResponse => {
+                this.rowData = successResponse
+            })
+        },
+        getConsole () {
+            console.log(this.rowData)
+        }
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import ShopManagement from '../components/app/ShopManagement'
 
 const routes = [
   {
@@ -8,14 +9,19 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/ShopManagement",
+    name: "Shop Management",
+    component: ShopManagement
   }
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
 ];
 
 const router = createRouter({
